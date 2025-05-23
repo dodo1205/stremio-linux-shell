8e206e6f0cf7ec2c2c12db25b623b151fae60c67
Tim <tymmesyde@gmail.com>
2025-05-23 09:53:15 +0200

    build(flatpak): reduce package size

--- build-aux/flatpak/build.sh
+++ build-aux/flatpak/build.sh
@@ -5,6 +5,5 @@ cwd="build-aux/flatpak"
 
 python3 $cwd/flatpak-builder-tools/cargo/flatpak-cargo-generator.py Cargo.lock -o $cwd/cargo-sources.json
 
-flatpak-builder --force-clean $cwd/build $package_id.json
-flatpak build-export $cwd/repo $cwd/build
+flatpak-builder --repo=$cwd/repo --force-clean $cwd/build $package_id.json
 flatpak build-bundle $cwd/repo $cwd/$package_id.flatpak $package_id
